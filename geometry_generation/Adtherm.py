339e149aed4bb6630317c9db0836b1ec05078d31
kirkbadger18 <kirk_badger@brown.edu>
2023-08-11 14:59:52 -0400

    Added some outlining as to how this will work

--- geometry_generation/Adtherm.py
+++ geometry_generation/Adtherm.py
@@ -1,26 +1,37 @@
 ###  Import some stuff ###
-
-
-
-
+import os
+import sys
+import system_train as syst
+import numpy as np
+import sobol_seq
+from sobol_seq import i4_sobol, i4_sobol_generate
 #########################
-
 
 
 class Adtherm:
 
-	def __init__(atoms,indeces,n_gauss=5000,n_sobol=0,n_random=0,relax_internals=False):
-
+	def __init__(atoms,indices,n_gauss=5000,n_sobol=0,n_random=0,relax_internals=False):
+        
+        self.atoms=atoms
+        self.adsorbate=self.atoms[indices].copy()
 		self.N_atoms=len(atoms)
 		self.N_gauss=n_gauss
 		self.N_sobol=n_sobol
 		self.N_random=n_random
 
-	def run():
-		geometry_generate(atoms,indices,'hessian')
-		for img in images:
-			img.get_potential_energy()
+    def geometry_generate(method,hessian=None)
+        ads=self.adsorbate
 
+
+
+    def run():
+		H_list=geometry_generate('hessian')
+		for img in H_list:
+			img.get_potential_energy()
+        H=calculate_H(H_list)
+        np.savetxt(H,'Rigid_H.txt')
+
+        gauss_list=geometry_generate('gauss',hessian=H)
 	
 
 
